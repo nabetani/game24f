4c4c68cfede4097497c99502c56aa648da872d53
Nabetani <takenori@nabetani.sakura.ne.jp>
2024-10-10 19:19:21 +0900

    表示調整

--- tfactory/src/Field.tsx
+++ tfactory/src/Field.tsx
@@ -9,31 +9,35 @@ import { Updater } from 'use-immer';
 import { Upcoming } from "@mui/icons-material";
 
 
-function AddBuildingUI(p: { world: G.World, updateWorld: Updater<G.World>, fieldObj: G.FieldObj }): JSX.Element {
+function AddBuildingUI(p: {
+  world: G.World,
+  updateWorld: Updater<G.World>,
+  fieldObj: G.FieldObj,
+  closer: () => void
+}): JSX.Element {
   const [param, setParam] = React.useState<G.BuildParam>(G.defaultBuildParam(p.world))
   const bs = G.bulidState(p.world, param, p.fieldObj)
-  return <>  <Mui.FormControl>
-    <Mui.FormLabel id="btype-selector">建物の種類</Mui.FormLabel>
-    <Mui.RadioGroup
-      row
-      aria-labelledby="btype-selector"
-      name="btype-selector-buttons-group"
-    >
-      <Mui.FormControlLabel
-        checked={param.toBiuld == G.FieldObjKind.factory}
-        value="factory" control={<Mui.Radio />} label="工場" onClick={
-          () => setParam({ ...param, toBiuld: G.FieldObjKind.factory })} />
-      <Mui.FormControlLabel
-        checked={param.toBiuld == G.FieldObjKind.pLabo}
-        value="plabo" control={<Mui.Radio />} label="生産技術研究所" onClick={
-          () => setParam({ ...param, toBiuld: G.FieldObjKind.pLabo })} />
-      <Mui.FormControlLabel
-        checked={param.toBiuld == G.FieldObjKind.bLabo}
-        value="blabo" control={<Mui.Radio />} label="基礎研究所" onClick={
-          () => setParam({ ...param, toBiuld: G.FieldObjKind.bLabo })} />
-    </Mui.RadioGroup>
-  </Mui.FormControl>
-    <Mui.FormControl>
+  return <>
+    <Mui.Box><Mui.FormControl size="small">
+      <Mui.FormLabel id="btype-selector">建物の種類</Mui.FormLabel>
+      <Mui.RadioGroup
+        row
+        aria-labelledby="btype-selector"
+        name="btype-selector-buttons-group"
+      >
+        <Mui.FormControlLabel
+          checked={param.toBiuld == G.FieldObjKind.factory}
+          value="factory" control={<Mui.Radio />} label="工場" onClick={
+            () => setParam({ ...param, toBiuld: G.FieldObjKind.factory })} />
+        <Mui.FormControlLabel
+          checked={param.toBiuld == G.FieldObjKind.pLabo}
+          value="plabo" control={<Mui.Radio />} label="生産技術研究所" onClick={
+            () => setParam({ ...param, toBiuld: G.FieldObjKind.pLabo })} />
+        <Mui.FormControlLabel
+          checked={param.toBiuld == G.FieldObjKind.bLabo}
+          value="blabo" control={<Mui.Radio />} label="基礎研究所" onClick={
+            () => setParam({ ...param, toBiuld: G.FieldObjKind.bLabo })} />
+      </Mui.RadioGroup>
       <Mui.FormLabel id="bsize-selector">建物のサイズ</Mui.FormLabel>
       <Mui.RadioGroup
         row
@@ -73,7 +77,7 @@ function AddBuildingUI(p: { world: G.World, updateWorld: Updater<G.World>, field
       <Mui.Stack direction={"row"} gap={3}>
         <Mui.Typography>コスト: {bs.cost} T </Mui.Typography>
         <Mui.Typography> 工期: {bs.duration} w</Mui.Typography>
-        <Mui.Typography> 能力: {bs.power} w</Mui.Typography>
+        <Mui.Typography> 能力: {bs.power}</Mui.Typography>
       </Mui.Stack>
       <Mui.Button variant="contained"
         disabled={!bs.canBuild}
@@ -81,27 +85,34 @@ function AddBuildingUI(p: { world: G.World, updateWorld: Updater<G.World>, field
           p.updateWorld((w: G.World) => {
             G.addBuilding(w, p.fieldObj, param)
           })
+          p.closer()
         }}
       >着工</Mui.Button>
     </Mui.FormControl>
+    </Mui.Box >
   </>
 }
 
-function CellClickUI(p: { world: G.World, updateWorld: Updater<G.World>, fieldObj: G.FieldObj }): JSX.Element {
+function CellClickUI(p: {
+  world: G.World,
+  updateWorld: Updater<G.World>,
+  fieldObj: G.FieldObj,
+  closer: () => void
+}): JSX.Element {
   const [value, setValue] = React.useState('1');
   const handleChange = (_: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
   return <MuiL.TabContext value={value}>
     <Mui.Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-      <TabList onChange={handleChange} aria-label="lab API tabs example">
+      <TabList onChange={handleChange} aria-label="lab API tabs example" >
         <Mui.Tab label="建築" value="1" />
         <Mui.Tab label="撤去" value="2" />
         <Mui.Tab label="強化" value="3" />
       </TabList>
     </Mui.Box>
     <TabPanel value="1">
-      <AddBuildingUI world={p.world} updateWorld={p.updateWorld} fieldObj={p.fieldObj} />
+      <AddBuildingUI world={p.world} updateWorld={p.updateWorld} fieldObj={p.fieldObj} closer={p.closer} />
     </TabPanel>
     <TabPanel value="2">Item Two</TabPanel>
     <TabPanel value="3">Item Three</TabPanel>
@@ -109,26 +120,27 @@ function CellClickUI(p: { world: G.World, updateWorld: Updater<G.World>, fieldOb
 }
 
 function FieldObj(p: { world: G.World, updateWorld: Updater<G.World>, fieldObj: G.FieldObj }): JSX.Element {
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null | undefined>(null);
   const fo = p.fieldObj
   const W = 70
+  const H = 50
   const s: React.CSSProperties = {
     left: fo.area.x * W,
-    top: fo.area.y * W,
+    top: fo.area.y * H,
     minWidth: fo.area.w * W,
-    maxWidth: fo.area.h * W,
-    minHeight: fo.area.h * W,
-    maxHeight: fo.area.w * W,
+    maxWidth: fo.area.w * W,
+    minHeight: fo.area.h * H,
+    maxHeight: fo.area.h * H,
   }
   const col = new Map<G.FieldObjKindType, string>([
-    [G.FieldObjKind.none, "black"],
+    [G.FieldObjKind.none, "#eee"],
     [G.FieldObjKind.bLabo, "darkorange"],
     [G.FieldObjKind.pLabo, "red"],
     [G.FieldObjKind.factory, "gray"],
     [G.FieldObjKind.house, "green"],
   ]).get(fo.kind)
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget?.parentElement?.parentElement);
   };
   const handleClose = () => {
     setAnchorEl(null);
@@ -138,7 +150,12 @@ function FieldObj(p: { world: G.World, updateWorld: Updater<G.World>, fieldObj: 
   return <div className={"cell"} style={s}>
     <Mui.Button
       aria-describedby={id} fullWidth={false}
-      sx={{ backgroundColor: col }}
+      sx={{
+        backgroundColor: col,
+        borderWidth: "3px",
+        borderColor: "black",
+        borderStyle: (open ? "solid" : "none"),
+      }}
       variant="contained"
       onClick={handleClick}>{`${fo.kind}`}</Mui.Button>
     <Mui.Popover
@@ -150,7 +167,7 @@ function FieldObj(p: { world: G.World, updateWorld: Updater<G.World>, fieldObj: 
         vertical: 'bottom',
         horizontal: 'left',
       }}
-    ><CellClickUI world={p.world} updateWorld={p.updateWorld} fieldObj={p.fieldObj} /></Mui.Popover></div >
+    ><CellClickUI world={p.world} updateWorld={p.updateWorld} fieldObj={p.fieldObj} closer={() => { setAnchorEl(null) }} /></Mui.Popover></div >
 }
 
 class Field extends React.Component<{ world: G.World, updateWorld: Updater<G.World> }, {}> {
